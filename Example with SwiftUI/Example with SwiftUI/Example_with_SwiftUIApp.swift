0438db2bf442bf4398ab4ff83a9b34e58be6efd7
Frank Mortensen <fmortens@me.com>
2021-01-09 15:34:00 +0100

    Add project files for Swift UI

---
+++ Example with SwiftUI/Example with SwiftUI/Example_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_with_SwiftUIApp.swift
+//  Example with SwiftUI
+//
+//  Created by Frank Mortensen on 09/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Example_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
