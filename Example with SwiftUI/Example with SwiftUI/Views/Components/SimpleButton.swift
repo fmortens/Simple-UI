af3cbf38b919d543a61dc7a88191333980039950
Frank Mortensen <fmortens@me.com>
2021-01-10 00:38:09 +0100

    Modify button to take available width of parent

--- Example with SwiftUI/Example with SwiftUI/Views/Components/SimpleButton.swift
+++ Example with SwiftUI/Example with SwiftUI/Views/Components/SimpleButton.swift
@@ -13,6 +13,7 @@ struct SimpleButton: View {
 
     var body: some View {
         Button(label, action: action)
+            .frame(maxWidth: .infinity)
             .padding(.vertical, 10)
             .padding(.horizontal, 20)
             .background(Color.blue)
@@ -25,14 +26,21 @@ struct SimpleButton: View {
 struct SimpleButton_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            SimpleButton(label: "Some label") {
+            SimpleButton(label: "Too small") {
                 // Some action
             }
-            SimpleButton(label: "Very large text 1234") {
+            .previewLayout(.fixed(width: 100, height: 80))
+
+            SimpleButton(label: "Normal width") {
                 // Some action
             }
+            .previewLayout(.fixed(width: 200, height: 80))
+
+            SimpleButton(label: "Too great width") {
+                // Some action
+            }
+            .previewLayout(.fixed(width: 400, height: 80))
         }
         .padding()
-        .previewLayout(.sizeThatFits)
     }
 }
