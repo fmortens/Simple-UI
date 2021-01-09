c13679271e25ac14ca7bfaecf622429f8f29dca8
Frank Mortensen <fmortens@me.com>
2021-01-09 16:15:34 +0100

    Add simple viewmodel and method for picking random string

--- Example with SwiftUI/Example with SwiftUI/ContentView.swift
+++ Example with SwiftUI/Example with SwiftUI/ContentView.swift
@@ -7,27 +7,64 @@
 
 import SwiftUI
 
+class ViewModel: ObservableObject {
+    let availableTexts: [String] = [
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Lorem ipsum dolor sit amet.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    ]
+
+    @Published var currentText: String = ""
+
+    func pickRandomCurrentText() {
+        let index = Int.random(in: 0..<availableTexts.count)
+        currentText = availableTexts[index]
+    }
+
+}
+
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+
     var body: some View {
         NavigationView {
-            VStack(alignment: .center, spacing: 10) {
-                Image("Background").resizable()
-                Text("Lorem ipsum")
+            ZStack {
+                Color.gray.ignoresSafeArea()
+                ScrollView {VStack(alignment: .center, spacing: 10) {
+                Image("Background")
+                    .resizable()
+                    .frame(maxHeight: 200)
+                    .cornerRadius(10)
+                    .padding(.all, 20)
+                    Text(viewModel.currentText)
+                    .padding(.all, 20)
                 Divider()
                 HStack {
                     Button("Større tekst") {
                         // something
                     }
+                    .padding(.all, 20)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
                     Spacer()
                     Button("Bytt ut text") {
-                        // something else
+                        viewModel.pickRandomCurrentText()
                     }
+                    .padding(.all, 20)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
                 }
-                .padding()
+                .padding(.all, 20)
                 Divider()
                 Spacer()
-            }
+            }.background(Color.white)
+                }}
             .navigationBarTitle("Demo")
+                .onAppear {
+                    viewModel.pickRandomCurrentText()
+                }
         }
     }
 }
