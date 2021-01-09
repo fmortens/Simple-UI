fbd2b0a5b61b21b448485f391467a71afdf7000f
Frank Mortensen <fmortens@me.com>
2021-01-09 17:24:09 +0100

    Add logic for increasing font size
    Start on 16 with the system font

--- Example with SwiftUI/Example with SwiftUI/ContentView.swift
+++ Example with SwiftUI/Example with SwiftUI/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 class ViewModel: ObservableObject {
+    var currentIndex = 0
     let availableTexts: [String] = [
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         "Lorem ipsum dolor sit amet.",
@@ -15,10 +16,24 @@ class ViewModel: ObservableObject {
     ]
 
     @Published var currentText: String = ""
+    @Published var fontSize: CGFloat = 16
 
     func pickRandomCurrentText() {
-        let index = Int.random(in: 0..<availableTexts.count)
-        currentText = availableTexts[index]
+        let newIndex = Int.random(in: 0..<availableTexts.count)
+
+        // Make sure we get a new index
+        if newIndex != currentIndex {
+            currentIndex = newIndex
+        } else {
+            // Try again
+            pickRandomCurrentText()
+        }
+
+        currentText = availableTexts[currentIndex]
+    }
+
+    func increaseFontSize() {
+        fontSize += 1.0
     }
 
 }
@@ -38,6 +53,7 @@ struct ContentView: View {
                             .cornerRadius(10)
 
                         Text(viewModel.currentText)
+                            .font(.system(size: viewModel.fontSize))
                             .onAppear {
                                 viewModel.pickRandomCurrentText()
                             }
@@ -46,7 +62,7 @@ struct ContentView: View {
 
                         HStack {
                             Button("Større tekst") {
-                                // something
+                                viewModel.increaseFontSize()
                             }
                             .padding(.vertical, 10)
                             .padding(.horizontal, 20)
