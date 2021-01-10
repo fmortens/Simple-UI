1fa51ce569951abf08ff9949a771857a38f50fe3
Frank Mortensen <fmortens@me.com>
2021-01-10 18:13:10 +0100

    Extend viewmodel with image name
    Also use viewmodel from view controller

--- Example width UIKit/Example width UIKit/Controller/ViewController.swift
+++ Example width UIKit/Example width UIKit/Controller/ViewController.swift
@@ -11,11 +11,12 @@ class ViewController: UITableViewController {
 
     @IBOutlet var demoTableView: UITableView!
 
+    var viewModel: ViewModel?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
 
-        //parent?.navigationController?.navigationBar.barTintColor = UIColor.darkGray
+        viewModel = ViewModel()
 
         demoTableView.delegate = self
     }
@@ -35,10 +36,14 @@ extension ViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+        guard let viewModel = viewModel else {
+            fatalError("Missing viewmodel!")
+        }
+        
         switch indexPath.row {
             case 0:
                 let cell = demoTableView.dequeueReusableCell(withIdentifier: "ImageCell", for: indexPath) as! ImageCell
-                cell.demoImageView.image = UIImage(named: "Background")
+                cell.demoImageView.image = UIImage(named: viewModel.imageName)
                 return cell
             default: fatalError("Unknown cell type!")
         }
